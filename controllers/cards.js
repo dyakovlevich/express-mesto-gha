d45e488d89ef96c0df71d01caf0f343469c33a66
Dmitry Yakovlevich <dyakovlevich@yandex.ru>
2022-08-09 19:09:47 +0000

    add cards

--- controllers/cards.js
+++ controllers/cards.js
@@ -2,8 +2,8 @@ const Card = require('../models/card');
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
-  const userId = req.user._id;
-  Card.create({ name, link, userId })
+  const owner = req.user._id;
+  Card.create({ name, link, owner })
     .then(card => res.send({ data: card }))
     .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
 };
