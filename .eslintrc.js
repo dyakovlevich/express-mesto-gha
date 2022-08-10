2ef0c70e3b27d9973bc1e256e56bdc667c4ecb77
Dmitry Yakovlevich <dyakovlevich@yandex.ru>
2022-08-10 09:02:30 +0000

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-    "extends": [
-        "airbnb-base",
-    ],
-    "rules": {
-      "no-console": "off",
-      "no-underscore-dangle": ["error", { "allow": ["_id"] }]
-    }
-}
+  extends: [
+    'airbnb-base',
+  ],
+  rules: {
+    'no-console': 'off',
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
+  },
+};
