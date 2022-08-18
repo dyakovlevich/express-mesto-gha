c65df0d5a82df8e8a389f0b7154a028ed24a7ed0
Dmitry Yakovlevich <dyakovlevich@yandex.ru>
2022-08-18 19:52:27 +0000

    fix regexp

--- utils/isLink.js
+++ utils/isLink.js
@@ -1,3 +1,3 @@
-const isLinkRegex = /^https?:\/\/(www\.)?[a-zA-Z0-9-]{2,}\.[a-zA-Z0-9]{2,}[a-zA-Z0-9\-._~:/?#[\]@!$&'()*+,;=]*/g;
+const isLinkRegex = /^https?:\/\/(www\.)?[a-zA-Z0-9-]{2,}\.[a-zA-Z0-9]{2,}[a-zA-Z0-9\-._~:/?#[\]@!$&'()*+,;=]*/;
 const isLink = (link) => isLinkRegex.test(link);
 module.exports = { isLink, isLinkRegex };
