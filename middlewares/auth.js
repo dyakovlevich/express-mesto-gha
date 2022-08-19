870507c0518055d94451fb841002bf3ca26f9d69
Dmitry Yakovlevich <dyakovlevich@yandex.ru>
2022-08-19 10:03:37 +0000

    fix login2

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,42 +5,42 @@ const jwt = require('jsonwebtoken');
 
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
-// module.exports = (req, res, next) => {
-//   const token = req.cookies.jwt;
-//
-//   if (!token) {
-//     return next(new UnauthorizedError('Необходима авторизация. Отсутствует токен.'));
-//   }
-//   let payload;
-//
-//   try {
-//     payload = jwt.verify(token, JWT_SECRET);
-//   } catch (err) {
-//     next(new UnauthorizedError('Необходима авторизация'));
-//   }
-//
-//   req.user = payload; // записываем пейлоуд в объект запроса
-//
-//   return next(); // пропускаем запрос дальше
-// };
-
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
+  const token = req.cookies.jwt;
 
-  if (!authorization || !authorization.startsWith('Bearer ')) {
+  if (!token) {
     return next(new UnauthorizedError('Необходима авторизация. Отсутствует токен.'));
   }
-
-  const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
-    payload = jwt.verify(token, 'JWT_SECRET');
+    payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    return next(new UnauthorizedError('Необходима авторизация. Отсутствует токен.'));
+    next(new UnauthorizedError('Необходима авторизация'));
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
 
   return next(); // пропускаем запрос дальше
 };
+
+// module.exports = (req, res, next) => {
+//   const { authorization } = req.headers;
+//
+//   if (!authorization || !authorization.startsWith('Bearer ')) {
+//     return next(new UnauthorizedError('Необходима авторизация. Отсутствует токен.'));
+//   }
+//
+//   const token = authorization.replace('Bearer ', '');
+//   let payload;
+//
+//   try {
+//     payload = jwt.verify(token, JWT_SECRET);
+//   } catch (err) {
+//     return next(new UnauthorizedError('Необходима авторизация. Отсутствует токен.'));
+//   }
+//
+//   req.user = payload; // записываем пейлоуд в объект запроса
+//
+//   return next(); // пропускаем запрос дальше
+// };
